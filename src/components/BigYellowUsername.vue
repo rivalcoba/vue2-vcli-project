d4e9f5528a558be57b8a1b6c808af0771a139ae4
rivalcoba <ivan.rivalcoba@gmail.com>
2021-07-02 12:54:12 -0500

    Ámbito en estilos

---
+++ src/components/BigYellowUsername.vue
@@ -0,0 +1,21 @@
+<template>
+  <p class="username">{{ username }}</p>
+</template>
+
+<script>
+export default {
+  props: {
+    username: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style scoped>
+.username {
+  font-size: 50pt;
+  color: #dcae1d;
+}
+</style>
